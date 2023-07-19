45cfe300b70ab5deb2f7c978cf204c58185b89be
Zaryab Ikram <zaryabsheikh0@gmail.com>
2023-07-19 21:23:07 +0500

    till pagination and filters

---
+++ src/features/product/productAPI.js
@@ -0,0 +1,37 @@
+export function fetchAllProducts() {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchProductsByFilters(filter, sort, pagination) {
+  // filter = {"category":"smartphone"}
+  // TODO : on server we will support multi values
+
+  let queryString = "";
+  for (let key in filter) {
+    const CategoryValue = filter[key];
+    if (CategoryValue.length > 0) {
+      const lastCategoryValue = CategoryValue[CategoryValue.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
+  }
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
+  }
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    const totalItems = await response.headers.get("X-Total-Count")
+    resolve({ data:{products:data,totalItems:+totalItems} });
+  });
+}
